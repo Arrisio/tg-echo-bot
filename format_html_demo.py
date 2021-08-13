d0620f62c59173f9670fd6ef0043ffb4d4705c2f
Dmitriy Suchkov <Account.log@yandex.ru>
2021-08-13 11:14:14 +0300

    resend wrogn html messages

--- format_html_demo.py
+++ format_html_demo.py
@@ -4,6 +4,7 @@
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 
-print(format_html('{}', mark_safe('Пицца с <i>сыром</i>')))
-print(format_html('{}', mark_safe('Пицца с <i>сыром')))
+print(format_html('{}', mark_safe('Пицца с <i>сыром</i>')), '-OK')
+print(format_html('{}', mark_safe('Пицца с <i#>сыром')), '-OK')
+print(format_html('{}', mark_safe('Пицца с <i>сыром')), '-BAD')
 
